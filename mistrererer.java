e831863b3a93ea74c9c424d34132c8079972e6cb
vivekdamani <56884865+vivekdamani@users.noreply.github.com>
2019-10-23 18:49:53 +0530

    Add files via upload

---
+++ mistrererer.java
@@ -0,0 +1,17 @@
+package Squizz;
+
+/**
+ *
+ * @author admin
+ */
+import acm.program.DialogProgram;
+public class tempo extends DialogProgram
+{
+    public void run(String msg)
+    {
+    
+        println(msg);
+
+    }
+}
+
